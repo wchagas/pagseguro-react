b089fea0867c43858da11611401a63d3813a294c
Wander Lima <wander@wanderlima.com>
2019-02-02 20:23:06 -0200

    npm packages and adjustments update

--- webpack/development.js
+++ webpack/development.js
@@ -2,57 +2,40 @@ const path = require("path");
 const webpack = require('webpack');
 
 module.exports = {
-    
+
 	entry: [
-        path.resolve(__dirname, '../example/index.js')
-    ],
+		path.resolve(__dirname, '../example/index.js')
+	],
 
-    devtool: 'inline-source-map',
+	devtool: 'inline-source-map',
 
-    mode: 'development',
-    
+	mode: 'development',
+
 	resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            'pagseguro-react': path.resolve(__dirname, '../src/')
-        }
-    },
-	
+		extensions: ['.js', '.jsx'],
+		alias: {
+			'pagseguro-react': path.resolve(__dirname, '../src/')
+		}
+	},
+
 	output: {
 		filename: 'index.js',
 		sourceMapFilename: '[file].map',
 		publicPath: 'http://localhost:9090/assets'
 	},
 
-    module: {
-        rules: [
-            {
-                test: /\.js[x]?$/,
-                loader: 'babel-loader',
-                exclude: /(node_modules|bower_components)/,
-				options: {
-                    presets: [
-                        ['env', {
-                            useBuiltIns: 'usage',
-                            modules: false,
-                        }],
-                        "react",
-                    ],
-                    plugins: [
-                        "babel-plugin-styled-components",
-                        "transform-object-rest-spread",
-                        "transform-class-properties",
-                        ["transform-es2015-modules-commonjs", {
-                            "allowTopLevelThis": true
-                        }]
-                    ]
-                }
-            },
-            {
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: "babel-loader",
+				exclude: /node_modules/
+			},
+			            {
               test: /\.css$/,
               use: [ 'style-loader', 'css-loader' ]
             },
-            {
+			{
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
                 exclude: [/fonts/],
                 use: {
@@ -61,41 +44,12 @@ module.exports = {
                         name: "img/[name].[hash:5].[ext]",
                     },
                 },
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                exclude: [/img/],
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        name: "fonts/[name].[hash:5].[ext]",
-                    },
-                },
-            },
-            {
-                test: /\.(pdf)(\?.*)?$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: "/pdf/[name].[ext]",
-                    },
-                },
-            },
-            {
-                test: /\.(ico)(\?.*)?$/,
-                exclude: [/img/],
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        name: "[name].[ext]",
-                    },
-                },
             }
-        ]
-    },
-   
+		]
+	},
+
 	devServer: {
-        inline: true,
+		inline: true,
 		host: 'localhost',
 		port: 9090,
 		contentBase: path.join(__dirname, '../example'),
@@ -104,14 +58,14 @@ module.exports = {
 		}
 	},
 
-    plugins: [
-        new webpack.DefinePlugin({
-            __DEV__: JSON.stringify(true),
-            'process.env.NODE_ENV': JSON.stringify('development'),
-        }),
-        new webpack.LoaderOptionsPlugin({
-            debug: true
-        })
-    ]
+	plugins: [
+		new webpack.DefinePlugin({
+			__DEV__: JSON.stringify(true),
+			'process.env.NODE_ENV': JSON.stringify('development'),
+		}),
+		new webpack.LoaderOptionsPlugin({
+			debug: true
+		})
+	]
 
 }
